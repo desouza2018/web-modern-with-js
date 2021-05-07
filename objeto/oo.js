815a6ad59c63c18ba499828fc56709e8d26b3bf8
desouza2018 <56815618+desouza2018@users.noreply.github.com>
2021-05-07 20:53:59 -0300

    Introdução a POO

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+//CÓDIGO NÃO EXECUTÁVEL
+
+//PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2, 
+    valor3,
+    processamento(){
+        //....
+    }
+}
+
+objeto.processamento()// O FOCO PASSOU A SER O OBJETO
+
+//Princípios importantes:
+// 1 - Abstração
+// 2 - Encapsulamento
+// 3 - Herança(prototype)
+// 4 - Polimorfismo
+
