59b745f14cbaa5047a7f69760f7e39b48b642a2c
desouza2018 <desouzaadelson2018@outlook.com>
2021-06-30 20:10:04 -0300

    POO 2

--- objeto/oo.js
+++ objeto/oo.js
@@ -1,9 +1,14 @@
+/**
+ * Link: https://www.w3schools.com/js/js_object_definition.asp
+ * Link: https://www.w3schools.com/php/php_oop_what_is.asp
+ */
+
 //CÓDIGO NÃO EXECUTÁVEL
 
 //PROCEDURAL
 processamento(valor1, valor2, valor3)
 
-//OO
+/*OO
 objeto = {
     valor1,
     valor2, 
@@ -12,12 +17,35 @@ objeto = {
         //....
     }
 }
+*/
 
 objeto.processamento()// O FOCO PASSOU A SER O OBJETO
 
 //Princípios importantes:
 // 1 - Abstração
+/** 
+ * Abstração é pegar um objeto da vida real e traduzí-lo para dentro do sistema.
+ * Abstração foca sobre o comportamento observável de um objeto.
+*/
+
 // 2 - Encapsulamento
+/** 
+ * Encapsulamento é o processo de esconder todos os detalhes de um 
+ * objeto que não contribuem para as suas características essenciais
+ * O encapsulamento é o modo de dar ao objeto seu comportamento (caixa-preta),
+ *  que é o segredo da reutilização e confiabilidade.
+*/
 // 3 - Herança(prototype)
+/** 
+ * Herança é o mecanismo para expressar a similaridade entre Classes, 
+ * simplificando a definição de classes iguais que já foram definidas.
+ * 
+*/
 // 4 - Polimorfismo
+/** 
+ * Polimorfimos
+ *  Poli -> varias; Morfos -> formas.
+ * Significa que um objeto pode assumir diferentes formas.
+*/
+
 
