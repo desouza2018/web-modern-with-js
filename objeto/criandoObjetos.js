20cec3873623aea81edeff407795cb6a39926dd4
desouza2018 <desouzaadelson2018@outlook.com>
2021-07-01 20:27:28 -0300

    poo continuação

--- objeto/criandoObjetos.js
+++ objeto/criandoObjetos.js
@@ -15,15 +15,53 @@ console.log(obj2)
 // Funções construtoras
 function Produto (nome, preco, desc) {
     this.nome = nome//this torna esta variãvel pública.
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    }
 }
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2998.99, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+//Função Factory(função que fabrica algo)
+function criarFuncionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+  
+}
+
+const f1 = criarFuncionario('João', 7980, 4)
+const f2 = criarFuncionario('Maria', 1170, 1)
+console.log(f1.getSalario(), f2.getSalario())
+
+// Object.create
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+/** 
+ * Uma função famosa que retorna Objeto...
+ * JavaScript tem uma função incorporada para converter strings JSON em objetos JavaScript:
+ * JSON.parse() 
+ * */ 
+
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')//Formato JSON - string JSON
+// Veja o Link: https://www.w3schools.com/js/js_json_intro.asp
+console.log(fromJSON.info)
 
 /** This torna essa variável pública, ou seja, essa variável 
  * não está encapsulada dentro do objeto, não está escondida,
  * ou seja, ela está visível para fora do objeto. 
- * Então como agente trabalha com esse conceito de encapsulamento do JavaScript você tem que entender 
- * os níveis de visibilidade das coisas. 
+ * Então como agente trabalha com esse conceito de encapsulamento do JavaScript 
+ * você tem que entender os níveis de visibilidade das coisas. 
  * Você só tem JavaScript no nível global quando trabalha no nível do Browser.
- * Quando trabalha no nodejs, tem outros níveis:
+ * Quando você trabalha no nodejs, tem outros níveis:
  *  tem um nível global, tem um nível do próprio módulo que representa o arquivo e tem o 
  * nível da função.
  * Quando você trabalha no Browser e também no node, à partir das palavras let e const, 
