e28d5acdfe876e7b215f5f12825ac3419d56f60c
desouza2018 <desouzaadelson2018@outlook.com>
2021-08-12 19:15:21 -0300

    prototype chain starting

---
+++ objeto/heranca2.js
@@ -0,0 +1,4 @@
+// AULA 121
+//Link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+//Link: https://www.w3schools.com/js/js_object_prototypes.asp
+// Cadeia de protótipos (prototype chain)
