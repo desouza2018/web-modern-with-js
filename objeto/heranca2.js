5a864406b1dda3c7deca9e414fab2cc503e62dd0
desouza2018 <desouzaadelson2018@outlook.com>
2021-09-29 18:42:19 -0300

    aula 122 concluída

--- objeto/heranca2.js
+++ objeto/heranca2.js
@@ -1,4 +1,4 @@
-// AULA 121
+// AULA 122
 //Link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
 //Link: https://www.w3schools.com/js/js_object_prototypes.asp
 // Cadeia de protótipos (prototype chain)
